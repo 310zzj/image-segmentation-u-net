423afba6517289c6af1437886520f1ab9e4e51de
310zzj <310zzj@proton.me>
2021-11-19 18:27:12 +0000

    Linux 3.10.52

--- train.py
+++ train.py
@@ -11,3 +11,8 @@ from unet import UNet
 data_folder = "data"
 model_folder = Path("model")
 model_folder.mkdir(exist_ok=True)
+model_path = "model/unet-voc.pt"
+saving_interval = 10
+epoch_number = 100
+shuffle_data_loader = False
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
