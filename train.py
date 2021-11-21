5ac1de9d5dfb5cebd1e33844016160e485900655
310zzj <310zzj@proton.me>
2021-11-21 18:06:38 +0000

    fix demo

--- train.py
+++ train.py
@@ -16,3 +16,13 @@ saving_interval = 10
 epoch_number = 100
 shuffle_data_loader = False
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+transform = transforms.Compose([transforms.Resize((512, 512)), transforms.ToTensor(), transforms.Grayscale()])
+dataset = datasets.VOCSegmentation(
+    data_folder,
+    year="2007",
+    download=True,
+    image_set="train",
+    transform=transform,
+    target_transform=transform,
+)
