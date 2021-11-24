7d3b6d735a523d92ec5720b46c6ceaba554f67c1
310zzj <310zzj@proton.me>
2021-11-24 16:22:35 +0000

    fix hyperlink

--- unet/unet.py
+++ unet/unet.py
@@ -6,3 +6,9 @@ import torch.nn.functional as F
 
 class DoubleConv(nn.Module):
     def __init__(self, in_ch, out_ch):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
