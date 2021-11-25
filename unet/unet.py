6c918a6e30a6f24cbbd8a9337776a4cc1f4eee75
310zzj <310zzj@proton.me>
2021-11-25 10:40:09 +0000

    Change log update.

--- unet/unet.py
+++ unet/unet.py
@@ -12,3 +12,10 @@ class DoubleConv(nn.Module):
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_ch, out_ch, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
